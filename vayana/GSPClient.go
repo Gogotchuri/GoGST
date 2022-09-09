a373561d013f13eb0e3b14586a29c9bd7797db1f
Gogotchuri <igogo17@freeuni.edu.ge>
2022-09-09 20:08:24 +0400

    Enhances error handling adds get ewbs by date

--- vayana/GSPClient.go
+++ vayana/GSPClient.go
@@ -7,6 +7,7 @@ import (
 	vayanaTypes "github.com/gogotchuri/GoGST/vayana/types"
 	"net/http"
 	"strings"
+	"time"
 )
 
 const vayanaBasicEWBBase = "/basic/ewb/v1.0/v1.03"
@@ -25,17 +26,36 @@ type gspClient struct {
 func (c *gspClient) GetGSTINDetails(gstin string) (*vayanaTypes.GSTINDetails, error) {
 	endpoint := fmt.Sprintf("/basic/ewb/v1.0/v1.03/gstin-details/%s", gstin)
 	resp := &vayanaTypes.GSTINDetails{}
-	err, _ := c.sendRequest(request{
+	err, vErr := c.sendRequest(request{
 		method:   http.MethodGet,
 		endpoint: endpoint,
 		dest:     resp,
 	})
 	if err != nil {
+		if nErr := c.handleError(vErr); nErr != nil {
+			return nil, nErr
+		}
 		return nil, err
 	}
 	return resp, nil
 }
 
+func (c *gspClient) GetEWayBillsByDate(date time.Time) ([]types.EWBGetResponse, error) {
+	endpoint := vayanaBasicEWBBase + fmt.Sprintf("/by-date/%d/%d/%d", date.Day(), date.Month(), date.Year())
+	resp := make([]types.EWBGetResponse, 0)
+	err, vErr := c.sendRequest(request{
+		method:   http.MethodGet,
+		endpoint: endpoint,
+		dest:     &resp,
+	})
+	if err != nil {
+		if nErr := c.handleError(vErr); nErr != nil {
+			return nil, nErr
+		}
+		return resp, fmt.Errorf("failed to create ewaybill: %s", err)
+	}
+	return resp, nil
+}
 func (c *gspClient) CreateEWaybill(ewb types.EWBCreateRequest) (*types.EWBCreateResponse, error) {
 	endpoint := vayanaBasicEWBBase + "/gen-ewb"
 	resp := &types.EWBCreateResponse{}
@@ -46,13 +66,8 @@ func (c *gspClient) CreateEWaybill(ewb types.EWBCreateRequest) (*types.EWBCreate
 		dest:     resp,
 	})
 	if err != nil {
-		if vErr != nil {
-			if vErr.IsEWBError() {
-				messages := strings.Join(vErr.GetErrorMessages(), "; ")
-				err = fmt.Errorf("%s", messages)
-			} else if vErr.IsTokenExpired() {
-				return resp, vayanaTypes.ErrorTokenExpired
-			}
+		if nErr := c.handleError(vErr); nErr != nil {
+			return nil, nErr
 		}
 		return resp, fmt.Errorf("failed to create ewaybill: %s", err)
 	}
@@ -62,13 +77,16 @@ func (c *gspClient) CreateEWaybill(ewb types.EWBCreateRequest) (*types.EWBCreate
 func (c *gspClient) CancelEWaybill(cancel types.EWBCancelRequest) (*types.EWBCancelResponse, error) {
 	endpoint := fmt.Sprintf("%s/cancel", vayanaBasicEWBBase)
 	resp := &types.EWBCancelResponse{}
-	err, _ := c.sendRequest(request{
+	err, vErr := c.sendRequest(request{
 		method:   http.MethodPost,
 		endpoint: endpoint,
 		body:     cancel,
 		dest:     resp,
 	})
 	if err != nil {
+		if nErr := c.handleError(vErr); nErr != nil {
+			return nil, nErr
+		}
 		return nil, err
 	}
 	return resp, nil
@@ -77,13 +95,29 @@ func (c *gspClient) CancelEWaybill(cancel types.EWBCancelRequest) (*types.EWBCan
 func (c *gspClient) GetEWayBill(ewbNo string) (*types.EWBGetResponse, error) {
 	endpoint := fmt.Sprintf("%s/%s", vayanaBasicEWBBase, ewbNo)
 	resp := &types.EWBGetResponse{}
-	err, _ := c.sendRequest(request{
+	err, vErr := c.sendRequest(request{
 		method:   http.MethodGet,
 		endpoint: endpoint,
 		dest:     resp,
 	})
 	if err != nil {
+		if nErr := c.handleError(vErr); nErr != nil {
+			return nil, nErr
+		}
 		return nil, err
 	}
 	return resp, nil
+}
+
+func (c *gspClient) handleError(vErr *vayanaTypes.Error) error {
+	if vErr == nil {
+		return nil
+	}
+	if vErr.IsEWBError() {
+		messages := strings.Join(vErr.GetErrorMessages(), ";")
+		return fmt.Errorf("%s", messages)
+	} else if vErr.IsTokenExpired() {
+		return vayanaTypes.ErrorTokenExpired
+	}
+	return nil
 }
--- vayana/vayana_test.go
+++ vayana/vayana_test.go
@@ -5,11 +5,12 @@ import (
 	"github.com/gogotchuri/GoGST/types"
 	vayanaTypes "github.com/gogotchuri/GoGST/vayana/types"
 	"testing"
+	"time"
 )
 
 func getGSPClient() GoGST.GSPClient {
 	client, _ := NewDefaultClient(false, "3b5f5dfa-d244-40ed-bfb1-04d7820328b5")
-	client.SetActiveToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJBdXRoIiwidWlkIjoiOTYyM2Y5MDYtNmMyMy00NGFiLTg5MzUtYjU0OWZiMDM4ZjEwIiwibW9iIjoiKzkxLTU1NTU1NTUiLCJlbWwiOiJ0ZWNoK3ZheWFuYTFAa2VybmVsLmZpbmFuY2UiLCJpc3MiOiJ2LXRoZW8iLCJuYW1lIjoiSWxpYSBHb2dvdGNodXJpIiwib3JncyI6WyJ7XCJvaWRcIjpcIjNiNWY1ZGZhLWQyNDQtNDBlZC1iZmIxLTA0ZDc4MjAzMjhiNVwiLFwicHJpbVwiOnRydWUsXCJhZG1cIjp0cnVlLFwic2VydlwiOltcImdzcFwiLFwiZWFwaVwiLFwidnNcIl19Il0sImV4cCI6MTY2MjU0OTUwNCwiaWF0IjoxNjYyNTI3OTA0fQ.wdMwVr-y50hg8ShFzgACwmOWENAKGoMlnAtkeyAm-ViBKgB1X_XC_FZMXvyreGMEq2VqJeGSqU9nglguwqEjFXq2r0t38Bh_fmYOjSK7zW7HYa2sNAGCwc1-cpV62pYp6SrxcYf_2aqvxqOvIEBU4k9WYJ20VoO6w9j3kdJDaDxamluSvswgSCbjIcFSQQYZ9eXyURT9QIeUGVSmBNEdD7VjeHzJ-Dh3U_W-u9md81QDdbEoaPQ_35rIJeQvTT_O3V-_SDP56eNlX_IsZ9HpC3uh5UyM56TSkO8YD-zOfzewhR-QeOVJfL48_7k6J6HvfY8oe7S1Ps4vCY0_iTYltg")
+	client.SetActiveToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJBdXRoIiwidWlkIjoiOTYyM2Y5MDYtNmMyMy00NGFiLTg5MzUtYjU0OWZiMDM4ZjEwIiwibW9iIjoiKzkxLTU1NTU1NTUiLCJlbWwiOiJ0ZWNoK3ZheWFuYTFAa2VybmVsLmZpbmFuY2UiLCJpc3MiOiJ2LXRoZW8iLCJuYW1lIjoiSWxpYSBHb2dvdGNodXJpIiwib3JncyI6WyJ7XCJvaWRcIjpcIjNiNWY1ZGZhLWQyNDQtNDBlZC1iZmIxLTA0ZDc4MjAzMjhiNVwiLFwicHJpbVwiOnRydWUsXCJhZG1cIjp0cnVlLFwic2VydlwiOltcImdzcFwiLFwiZWFwaVwiLFwidnNcIl19Il0sImV4cCI6MTY2Mjc1ODgzNywiaWF0IjoxNjYyNzM3MjM3fQ.M2gR6xeOROPUk0-auzq2bmA_-g9d8N-4uRyxIaboQ1_vFuKvAYwSGwsVzncIbNREs-5iMENeRvlnj1tZE6QUwGKhKA66rrOiHhqoGMs39Vbg4VycL_Jd1HCY87bwhMswEW2_dZw7AqzDMnfbr9zvTFPlcdNp9iH1rlRasQsTce2xfe-2rZEMEfFZvfyb4apgH9KJJwa8976UMrQ0y9ylrmme3cnTfpB2YbLSUqgAqgH9sYwX6oey4w8VMe_hisE97Nv2adEPGHpMUAb-12IfxavV-CF7LGON2xnQ28XE8t1QKCTyuZCiSgofVvGgnUcBp6TVJraR1C6rNB8KI_nztg")
 	gspC, _ := client.CreateGSPClient("29AAACW4202F1ZM", "test_dlr228", "test_dlr228")
 	return gspC
 }
@@ -36,7 +37,7 @@ func TestClient_Authenticate(t *testing.T) {
 
 func TestClient_GetGSTINDetails(t *testing.T) {
 	gspC := getGSPClient()
-	resp, err := gspC.GetGSTINDetails("29AKLPM8755F1Z2")
+	resp, err := gspC.GetGSTINDetails("29AAACW4202F1ZM")
 	if err != nil {
 		t.Error(err)
 	}
@@ -50,7 +51,7 @@ func TestClient_GenerateEWB(t *testing.T) {
 			SupplyType:            "O",
 			SubSupplyType:         "1",
 			DocType:               "INV",
-			DocNo:                 "70001-454",
+			DocNo:                 "70001-4534",
 			DocDate:               "15/05/2022",
 			FromGstin:             "29AAACW4202F1ZM",
 			FromTrdName:           "welton",
@@ -110,4 +111,26 @@ func TestClient_GenerateEWB(t *testing.T) {
 		t.Error(err)
 	}
 	t.Logf("%+v", ewb)
+}
+
+func TestGspClient_CancelEWaybill(t *testing.T) {
+	gspC := getGSPClient()
+	res, err := gspC.CancelEWaybill(types.EWBCancelRequest{
+		CancelRemark:     "Other",
+		CancelReasonCode: 3,
+		EwayBillNo:       141002145189,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("%+v", res)
+}
+
+func TestGspClient_GetEWayBillsByDate(t *testing.T) {
+	gspC := getGSPClient()
+	res, err := gspC.GetEWayBillsByDate(time.Now())
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("%+v", res)
 }
